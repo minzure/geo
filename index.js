b0bf216aac5354ec5032552aa09e8208a1c734d7
Antimony51 <97737838+Antimony51@users.noreply.github.com>
2023-02-12 20:19:02 +0900

    Basic Setting

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 import * as THREE from 'three';
-const renderer = new THREE.WebGL1Renderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1 , 500);
-camera.position.set(0,0, 100);
-camera.lookAt(0,0,0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const screen = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+const scene = new THREE.Scene();
+
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 const points = [];
-points.push(new THREE.Vector3(-10, 0, 0));
-points.push(new THREE.Vector3(0, 10, 0));
-points.push(new THREE.Vector3(10,0, 0));
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
-alert("TEST")
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+renderer.render( scene, camera );``
